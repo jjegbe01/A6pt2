3f7c4a3dce1be8a27b5ccfe4e6df04bbc2e6f247
Julian Egbert <jjegbe01@louisville.edu>
2015-10-26 11:48:56 -0400

    Added initial project files.

---
+++ app/src/main/java/jjegbe01/a6pt2/MainActivity.java
@@ -0,0 +1,13 @@
+package jjegbe01.a6pt2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
