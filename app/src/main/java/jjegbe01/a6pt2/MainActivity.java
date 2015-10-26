7345f8ce86ba3a975ce54d28d4d61087506cbc1a
Julian Egbert <jjegbe01@louisville.edu>
2015-10-26 11:53:57 -0400

    Added first service and changed main activity

--- app/src/main/java/jjegbe01/a6pt2/MainActivity.java
+++ app/src/main/java/jjegbe01/a6pt2/MainActivity.java
@@ -1,7 +1,10 @@
 package jjegbe01.a6pt2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    // Method to start the service
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), HelloService.class));
+    }
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), HelloService.class));
     }
 }
